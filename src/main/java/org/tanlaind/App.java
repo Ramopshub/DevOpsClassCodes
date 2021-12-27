732e8294516b5834796fa3b8823b1b51010acde6
Ram <ram@gmail.com>
2021-12-27 04:23:47 +0000

    commit

---
+++ src/main/java/org/tanlaind/App.java
@@ -0,0 +1,13 @@
+package org.tanlaind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
